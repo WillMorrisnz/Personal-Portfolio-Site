7278bded234b4cfb056e0fa00cb4de702affc388
William Morris <ewm27@uclive.ac.nz>
2023-06-07 04:26:14 +1200

    Landing page, past work page

---
+++ components/ProjectCard.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const descriptionStyle = {
+  fontFamily: "Roboto",
+  fontWeight: 400,
+  fontSize: "1rem",
+  lineHeight: "1.5rem",
+  textAlign: "center",
+  color: "#FFFFFF",
+  width: "90%",
+  margin: "0 auto",
+  paddingTop: ".5em",
+};
+
+const ProjectCard = ({ imgSrc, title, description }) => (
+  <div
+    style={{
+      width: "100%",
+      maxWidth: "24em",
+      margin: "0 auto",
+      padding: "0",
+      minWidth: "14em",
+    }}
+  >
+    <img
+      src={imgSrc}
+      alt={title}
+      style={{
+        width: "100%",
+        borderRadius: "16px",
+        border: "0.5px solid #000000",
+        filter: "drop-shadow(0px 2px 5px rgba(0, 0, 0, 0.7))",
+        marginBottom: "1rem",
+        objectFit: "cover",
+        aspectRatio: 1 / 1,
+      }}
+    />
+    <h2 style={titleStyle}>{title}</h2>
+    <p style={descriptionStyle}>{description}</p>
+  </div>
+);
+
+ProjectCard.propTypes = {
+  imgSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default ProjectCard;
